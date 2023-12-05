33499e2a2f22cda1f0ff8c164bee79968fa10592
DanielAlexanderEastw <danieleastwell@hotmail.com>
2023-12-05 10:48:14 +0100

    ventana perfil alumno empezada

---
+++ src/main/java/com/example/retogrupalgestiontareas/controllers/StudentProfileView.java
@@ -0,0 +1,34 @@
+package com.example.retogrupalgestiontareas.controllers;
+
+public class StudentProfileView
+{
+    @javafx.fxml.FXML
+    private TextField txtName;
+    @javafx.fxml.FXML
+    private TextField txtSurname;
+    @javafx.fxml.FXML
+    private TextField txtDNI;
+    @javafx.fxml.FXML
+    private TextField txtDatebirth;
+    @javafx.fxml.FXML
+    private TextField txtPhone;
+    @javafx.fxml.FXML
+    private TextField txtEmail;
+    @javafx.fxml.FXML
+    private TextField txtCompany;
+    @javafx.fxml.FXML
+    private TextField txtTutor;
+    @javafx.fxml.FXML
+    private TextField txtDualHours;
+    @javafx.fxml.FXML
+    private TextField txtFCTHours;
+    @javafx.fxml.FXML
+    private TextField txtDualHoursLeft;
+    @javafx.fxml.FXML
+    private TextField txtFCTHoursLeft;
+    @javafx.fxml.FXML
+    private TextArea taObservations;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
