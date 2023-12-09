4c67130e27e4322f03568a333328b57efe0e359e
VICTOR <yo@casa.com>
2023-12-09 12:37:07 +0100

    Hibernate roto

--- src/main/java/com/example/retogrupalgestiontareas/domain/entities/company/Empresa.java
+++ src/main/java/com/example/retogrupalgestiontareas/domain/entities/company/Empresa.java
@@ -4,10 +4,14 @@ import java.io.Serializable;
 
 import com.example.retogrupalgestiontareas.domain.entities.user.Usuario;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
+@Entity
 @Table(name = "empresa")
 public class Empresa implements Serializable {
 
@@ -20,10 +24,24 @@ public class Empresa implements Serializable {
     private String email;
     private String observaciones;
 
+    @OneToMany(mappedBy = "empresa", fetch = FetchType.EAGER)
+    private List<Student> student;
+
     @ManyToOne
     @JoinColumn(name = "usuario_id")
     private Usuario usuario;
 
-
-
+    @Override
+    public String toString() {
+        return "Empresa{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", responsable='" + responsable + '\'' +
+                ", telefono=" + telefono +
+                ", email='" + email + '\'' +
+                ", observaciones='" + observaciones + '\'' +
+                ", student=" + student +
+                ", usuario=" + usuario +
+                '}';
+    }
 }
